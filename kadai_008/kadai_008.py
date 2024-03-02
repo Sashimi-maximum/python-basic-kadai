b0c298d04425524618f0532ca2c0ce89f2dda613
Sashimi-maximum <157551000+Sashimi-maximum@users.noreply.github.com>
2024-03-02 20:49:01 +0900

    Add files via upload

---
+++ kadai_008/kadai_008.py
@@ -0,0 +1,11 @@
+import random
+var = random.randint(0,15)
+
+if var%3 == 0 and var%5 == 0:
+  print("FizzBuzz")
+elif var%5 == 0:
+  print("Buzz")
+elif var%3 == 0:
+  print("Fizz")
+else:
+  print(var)
